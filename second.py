074be115ec9b237272e8f1525b8a271b6b404690
tanupat085 <54698281+tanupat085@users.noreply.github.com>
2020-04-20 19:11:04 +0700

    second

--- second.py
+++ second.py
@@ -1,17 +1,64 @@
 from tkinter import *
-from tkinter import ttk
+from tkinter import ttk, messagebox
+import csv
+
+def writetocsv(data):
+    with open('data.csv','a',newline='') as f:
+        fw = csv.writer(f)
+        fw.writerow(data)
+        print('CSV was writen')
+        
+
+def Addproduct():
+    calculate = int(Price.get())* int(Quantity.get())
+    text = "Product Name: {}\nPrice: {}\nQuantity: {}\nTotal: {}".format(Name.get(),Price.get(),Quantity.get(),calculate)
+    datatocsv = [Name.get(),Price.get(),Quantity.get(),calculate]
+    print(text)
+    Res.set(text)
+    writetocsv(datatocsv)
+    messagebox.showinfo('Adding..','Product was added')
 
 GUI = Tk()
+GUI.title('Python GUI : Ama Shop')
+GUI.geometry('700x500')
 
-GUI.title(' python GUI : aod shop ')
+#Label and Entry
+#----------------------Row 0---------------------
+LName = ttk.Label(GUI, text='Product Name',font=(None,18))
+LName.grid(row=0,column=0,padx=10,pady=10)
 
-GUI.geometry('500x500')
+Name = StringVar()
 
-#label และทางเข้า
-LName = ttk.Label(GUI , text='Product Name')
+EName = ttk.Entry(GUI, textvariable=Name,font=(None,18))
+EName.grid(row=0,column=1,padx=10,pady=10)
+#----------------------Row 1---------------------
+LPrice = ttk.Label(GUI, text='Price',font=(None,18))
+LPrice.grid(row=1,column=0,padx=10,pady=10)
 
-LName.grid(row=0 , column=0)
+Price = StringVar()
 
-#EName = tkk.Entry(GUI , textvariable=Name)
+EPrice = ttk.Entry(GUI, textvariable=Price,font=(None,18))
+EPrice.grid(row=1,column=1,padx=10,pady=10)
+#----------------------Row 2---------------------
+LQuantity = ttk.Label(GUI, text='Quantity',font=(None,18))
+LQuantity.grid(row=2,column=0,padx=10,pady=10)
+
+Quantity = StringVar()
+
+EQuantity = ttk.Entry(GUI, textvariable=Quantity,font=(None,18))
+EQuantity.grid(row=2,column=1,padx=10,pady=10)
+#----------------------Row 3---------------------
+# This is Button 
+BAdd = ttk.Button(GUI, text='Add',command=Addproduct)
+BAdd.grid(row=3,column=1,padx=10,pady=10)
+
+
+#Create Result Label (RL)
+Res = StringVar()
+Res.set('Result')
+
+#fg is foreground fg for Label Not ttk.Label
+RLRes = ttk.Label(GUI, textvariable=Res,font=(None,18,'bold'),foreground='green')
+RLRes.grid(row=4,column=1,padx=10,pady=10)
 
 GUI.mainloop()
